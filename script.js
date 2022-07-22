99188fc73642abce4b552dab8ab71e9573905104
jonathas <jonathasdossantos007@gmail.com>
2022-07-22 15:36:51 -0300

    Adicionando responsividade

---
+++ script.js
@@ -0,0 +1,25 @@
+const bars = document.querySelector("#bars");
+const bars02 = document.querySelector("#bars02");
+const utilities = document.querySelector(".utilities");
+const mainMenu = document.querySelector(".main-menu");
+
+bars.addEventListener('click', function(e){
+    if(e.target.classList.contains('bars-active')){
+        e.target.classList.remove('bars-active');
+    }
+    else{
+        e.target.classList.add('bars-active');
+    }
+    utilities.classList.toggle('utilities-active');
+});
+
+bars02.addEventListener('click', function(e){
+    if(e.target.classList.contains('bars-active')){
+        e.target.classList.remove('bars-active');
+    }
+    else{
+        e.target.classList.add('bars-active');
+    }
+    utilities.classList.toggle('utilities-active');
+    mainMenu.classList.toggle('main-menu-active');
+});
